449ab69bcc217a29449905e84a8e13c92e02efd0
Layne666 <791270330@qq.com>
2019-05-02 03:34:23 +0800

    新增个人中心头像管理页面

---
+++ src/main/java/site/layne666/gym/controller/CenterController.java
@@ -0,0 +1,18 @@
+package site.layne666.gym.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author layne666
+ * @date 2019/05/01
+ */
+@Controller
+@RequestMapping("/center")
+public class CenterController {
+    @RequestMapping("/photo")
+    public String center(){
+        return "center-photo";
+    }
+
+}
