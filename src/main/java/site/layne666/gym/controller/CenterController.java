525873e1d46f8e7aafdd57e6771e0d50243f8b92
Layne666 <791270330@qq.com>
2019-05-13 00:53:10 +0800

    修改头像管理模块

--- src/main/java/site/layne666/gym/controller/CenterController.java
+++ src/main/java/site/layne666/gym/controller/CenterController.java
@@ -3,11 +3,9 @@ package site.layne666.gym.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import site.layne666.gym.common.Consts;
 import site.layne666.gym.mapper.AccountMapper;
@@ -44,9 +42,40 @@ public class CenterController {
     @Autowired
     private CoachMapper coachMapper;
 
-    @RequestMapping("/photo")
-    public String center(){
+    @Value("${uploadPath}")
+    private String uploadPath;
+
+    @GetMapping("/photo")
+    public String photoGet(){
         return "center-photo";
+    }
+
+    @PostMapping("/photo")
+    @ResponseBody
+    public ApiResult photoPost(HttpSession session){
+        Account account = (Account)session.getAttribute("account");
+        Coach coach = account.getCoach();
+        String path = coach.getImg();
+        return new ApiResult(path);
+    }
+
+    @PostMapping("/photo/path")
+    @ResponseBody
+    public ApiResult path(String path, HttpSession session){
+        try{
+            Account account = (Account)session.getAttribute("account");
+            Coach coach = account.getCoach();
+            coach.setImg(path);
+            //更新数据库
+            coachMapper.updateCoach(coach);
+            //更新session
+            Account newAccount = accountMapper.getAccountByBh(account.getBh());
+            session.setAttribute("account",newAccount);
+            return new ApiResult(true,"头像上传成功");
+        }catch (Exception e){
+            log.error("头像上传失败",e);
+            return new ApiResult(true,"头像上传失败");
+        }
     }
 
     @RequestMapping("/data")
@@ -69,12 +98,13 @@ public class CenterController {
     public ApiResult upload(@RequestParam(value = "file") MultipartFile upload, HttpSession session, HttpServletResponse resp) {
         try{
             //要上传的文件路径
-            String path = System.getProperty("user.dir")+"//src//main//resources//static//upload";
+            String path = uploadPath;
             File file = new File(path);
             //获取原文件名
             String originalFilename = upload.getOriginalFilename();
             //获取原文件的后缀名
             String suffix  = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
+            //筛选jpg和png
             if(Consts.IMG_JPG.equals(suffix) || Consts.IMG_PNG.equals(suffix)){
                 Account account = ((Account) session.getAttribute("account"));
                 String img = UUIDUtil.getUUid();
@@ -84,9 +114,11 @@ public class CenterController {
                 Coach coach = account.getCoach();
                 //若之前的头像不是系统头像，则进行删除
                 if(!StringUtils.equals(Consts.IMG_DAFAULT,coach.getImg())){
-                    new File(path + "//" +coach.getImg()).delete();
+                    //删除旧图片
+                    new File(coach.getImg()).delete();
                 }
-                coach.setImg(filename);
+                //设置新的图片路径
+                coach.setImg(path + File.separator + filename);
                 //更新数据库
                 coachMapper.updateCoach(coach);
                 //更新session
